49303623249ebe0d9cad0300169bc244e6965f6f
Marc S. Brooks <marc@mbrooks.info>
2016-01-11 02:49:05 -0800

    Added controller to process SearchPage requests

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -7,77 +7,38 @@
 
 namespace Drupal\headless\Controller;
 
-use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Form\FormState;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpFoundation\Request;
+use Drupal\headless\HeadlessBase;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Serializer\Serializer;
 
 /**
  * Controller routine.
  */
-class SearchController extends ControllerBase {
+class SearchController extends HeadlessBase {
 
   /**
-   * Serializer instance.
-   *
-   * @var \Symfony\Component\Serializer\Serializer
-   */
-  protected $serializer;
-
-  /**
-   * General constructor.
-   *
-   * @param Serializer $serializer
-   */
-  public function __construct(Serializer $serializer) {
-    $this->serializer = $serializer;
-  }
-
-  /**
-   * @inheritdoc
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('serializer')
-    );
-  }
-
-  /**
-   * 
-   *
-   * @param Request $request
+   * Perform site-wide search.
    *
    * @return \Symfony\Component\HttpFoundation\Response
    *   Response represents an HTTP response.
    */
-  public function query(Request $request) {
-    $output = NULL;
+  public function query() {
+    $output = $this->submitForm('\Drupal\search\Form\SearchPageForm');
     $status = NULL;
 
-    $content = $request->getContent();
-    if ($content) {
-      $params = $this->serializer->decode($content, 'json');
+    // Form submission success.
+    if (isset($output['data'])) {
+      $status = Response::HTTP_ACCEPTED;
+    }
 
-      $form_state = (new FormState())->setValues($params);
-
-      \Drupal::formBuilder()->submitForm('\Drupal\search\Form\SearchPageForm', $form_state);
-
-      $errors = $form_state->getErrors();
-      if ($errors) {
-        $output = $this->serializer->serialize(array('error' => $errors), 'json');
-        $status = 400;
-      }
-      else {
-        $output = $this->serializer->serialize(array('data' => 'Hello'), 'json');
-        $status = 200;
-      }
+    // Errors exist.
+    elseif (isset($output['error'])) {
+      $status = Response::HTTP_BAD_REQUEST;
     }
     else {
-      $status = 500;
+      $status = Response::HTTP_FORBIDDEN;
     }
 
-    return new Response($output, $status);
+    // Send the response.
+    return new Response($this->serialize($output), $status);
   }
 }
