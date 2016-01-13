d0cedb0898a63b43e5ab10e1e4dec4bad7119337
Marc S. Brooks <marc@mbrooks.info>
2016-01-12 23:55:21 -0800

    Removed code that has been moved to HeadlessBase
    Removed JsonResponse include - use $this->response() instead

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -8,7 +8,6 @@
 namespace Drupal\headless\Controller;
 
 use Drupal\headless\HeadlessBase;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Controller routine.
@@ -22,23 +21,6 @@ class SearchController extends HeadlessBase {
    *   Response represents an HTTP response in JSON format.
    */
   public function query() {
-    $output = $this->submitForm('\Drupal\search\Form\SearchPageForm');
-    $status = NULL;
-
-    // Form submission success.
-    if (isset($output['data'])) {
-      $status = JsonResponse::HTTP_ACCEPTED;
-    }
-
-    // Errors exist.
-    elseif (isset($output['error'])) {
-      $status = JsonResponse::HTTP_BAD_REQUEST;
-    }
-    else {
-      $status = JsonResponse::HTTP_FORBIDDEN;
-    }
-
-    // Send the response.
-    return new JsonResponse($output, $status);
+    return $this->submitForm('\Drupal\search\Form\SearchPageForm');
   }
 }
